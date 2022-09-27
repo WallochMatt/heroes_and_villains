dc4d64ab5040903824df275d5268f025dd9fd06d
WallochMatt <matthewrwalloch@gmail.com>
2022-09-27 13:55:25 -0500

    feat: Registered apps with admin site + Created view funtions for GET PUT and DELETE

--- supers/views.py
+++ supers/views.py
@@ -1,7 +1,32 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SuperSerializer
+from .models import Super
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET'])# all with params
 def supers_list(request):
-    return Response('k')
+    supers = Super.objects.all()
+    serializer = SuperSerializer(supers, many=True)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def individual_super(request, pk):
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(super)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
