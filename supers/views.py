4895ff5478e5175bfdd5a414afd8ef9bbc312672
WallochMatt <matthewrwalloch@gmail.com>
2022-09-28 11:26:36 -0500

    feat: GET all supers now retruns a custom dictionary organized by hero and villain + Initialized debugging with launch.json

--- supers/views.py
+++ supers/views.py
@@ -5,20 +5,44 @@ from rest_framework.response import Response
 from .serializers import SuperSerializer
 from .models import Super
 
+
 # Create your views here.
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
-        
         morality = request.query_params.get('type')
-        
         supers = Super.objects.all()
         
         if morality:
             supers = supers.filter(super_type__type=morality)
+            serializer = SuperSerializer(supers, many = True)
+            return Response(serializer.data)
 
-        serializer = SuperSerializer(supers, many=True)
-        return Response(serializer.data)
+        
+        # heroes = supers.filter(super_type__type = 'Hero')
+        # serializer = SuperSerializer(heroes, many=True)
+        # return Response(serializer.data)
+       
+        else:#for supers.objects if super_type == 'hero'?
+            custom_dictionary = {'heroes':[], 'villains': []}
+
+            
+            heroes = supers.filter(super_type__type = 'Hero')
+            villains = supers.filter(super_type__type = 'Villain')
+            
+            for hero in heroes:
+                serializer = SuperSerializer(heroes, many = True)
+                custom_dictionary['heroes'] = serializer.data
+
+            for villain in villains:
+                serializer = SuperSerializer(villains, many = True)
+                custom_dictionary['villains'] = serializer.data
+
+
+            return Response(custom_dictionary)
+
+
+
 
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
