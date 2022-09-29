c0b8162dcedfadf608944b6ad6617a3148450e99
WallochMatt <matthewrwalloch@gmail.com>
2022-09-29 16:08:13 -0500

    feat: Added query params to GET all view method + Created a function for a fight between hero and villain (the qparams) + Added q params in comments for ease of use

--- supers/views.py
+++ supers/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
 from .serializers import PowerSerializer, SuperSerializer
 from .models import Power, Super
 from supers import serializers
@@ -13,13 +12,22 @@ from supers import serializers
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
+        hero = request.query_params.get('hero')
+        villain = request.query_params.get('villain')
         morality = request.query_params.get('type')
+        
         supers = Super.objects.all()
         
-        if morality:
+        if hero and villain:#api/supers/?hero=Spiderman&villain=Venom   or other super names
+            return Response(fight(hero, villain), status=status.HTTP_200_OK)
+
+
+        if morality: #api/supers/?type=Hero  or Villain
             supers = supers.filter(super_type__type=morality)
             serializer = SuperSerializer(supers, many = True)
             return Response(serializer.data)
+        
+        
         else:
             custom_dictionary = {'heroes':[], 'villains': []}
             pk = 1
@@ -68,3 +76,29 @@ def add_power_to_super(request, pk, set_power):
         super.power.add(new_power)
         serializer = SuperSerializer(super)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+def fight(hero, villain):
+    hero_fighter = get_object_or_404(Super, name=hero)
+    villain_fighter = get_object_or_404(Super, name=villain)
+
+    hero_points = 0
+    villain_points = 0
+
+    for ability in hero_fighter.power.all():
+        hero_points += 1
+    for ability in villain_fighter.power.all():
+        villain_points += 1
+
+    hero_serializer = serializer = SuperSerializer(hero_fighter)
+    villain_serializer = serializer = SuperSerializer(villain_fighter)
+
+    if hero_points > villain_points:
+        custom_dictionary = {'winner': hero_serializer.data, 'loser': villain_serializer.data}
+    elif villain_points > hero_points:
+        custom_dictionary = {'winner': villain_fighter, 'loser': hero_fighter}
+    elif hero_points == villain_points:
+        return "TIE"
+
+    return custom_dictionary
